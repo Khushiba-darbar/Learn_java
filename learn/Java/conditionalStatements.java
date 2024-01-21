9d1bf70eeb64c1a3ce454e413368331aca3bf503
Khushiba-darbar <145755690+Khushiba-darbar@users.noreply.github.com>
2024-01-21 17:45:52 +0000

    container config done

---
+++ learn/Java/conditionalStatements.java
@@ -0,0 +1,41 @@
+class conditionalStatements
+{
+    public static void main(String args[])
+    {
+        int x=8;
+        int y=14;
+
+        if(x>10 && y<=20)//11-20 ni range ma hoi to//
+
+            System.out.println("hello");
+    
+        else
+        
+            System.out.println("bye");
+        
+             //you can also use if alone but cant use else alone soo to use else there must be if//
+
+    /*if(x>y)
+    System.out.println("smallest number is "+x);// to print any value + is used
+    else
+   { System.out.println("smallest number is "+y);}
+   System.out.println("thankyou");*/
+    
+    int a=4;
+    int b=11;
+    int c=17;
+
+    if(a>b && a>c)
+    {System.out.println("smallest number is"+a);}
+   else if(b>a && b>c)
+   { System.out.println("smallest number is",+b);}
+    else{
+    System.out.println("smallest number is",+c);}
+    System.out.println("thankyou");
+
+
+    }
+
+
+
+}
